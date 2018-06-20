608486eca2ba9fe62d30a440c56746f7b4804f74
Struckdown <bfleysher@gmail.com>
2018-06-19 21:40:06 -0600

    New Room, Swap works over holes and through glass now
    New room has been added with a daunting new puzzle!
    Swap now works through holes in the ground and other such terrain.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -58,7 +58,7 @@ func controls_loop():
 		playerPos = SpriteNode.position
 		mousePos = get_global_mouse_position()
 		var space_state = get_world_2d().direct_space_state
-		var result = space_state.intersect_ray(position, mousePos, [self], collision_mask)
+		var result = space_state.intersect_ray(position, mousePos, [self], 5)
 		if result:
 			if result.collider.is_in_group("Enemy"):
 				swapPlaces(self, result.collider)
--- Scripts/TransferPoint.gd
+++ Scripts/TransferPoint.gd
@@ -3,6 +3,7 @@ extends Node2D
 # Required to pass editor info to collider child of object
 export var sceneName = "Room"
 export var transferGoal = "Goal"
+# Please see TransferZone.gd script
 
 func _ready():
 	# Called every time the node is added to the scene.
