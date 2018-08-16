9205e5ca15c11d0e03ffec603f39411ad8470b77
Struckdown <bfleysher@gmail.com>
2018-08-16 09:13:10 -0600

    Life Icons implemented
    Added life icons for player health
    Slightly updated the carry bullet puzzle
    DJ's Touhou Simulator is also in here, but not linked anywhere in the game
    Fixed a bug where you could pick up a piece of heart multiple times within a few frames for some reason
    Removed the unused progress bar that used to be health
    Restored the old collision circle to the player
    Updated the current player animations to better stay centered instead of moving off to the side for the vertical animations

--- Scripts/HealthPieceOfHeart.gd
+++ Scripts/HealthPieceOfHeart.gd
@@ -1,4 +1,6 @@
 extends "res://Scripts/HealthPickup.gd"
+var collected = false
+
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -6,17 +8,19 @@ func _ready():
 	pass
 
 func applyEffect(player):
-	player.piecesOfHeart += 1
-	emit_signal("collected")
-	print("Found a piece of heart!")
-	if player.piecesOfHeart == 3:
-		player.piecesOfHeart = 0
-		player.maxHealth += 1
-		player.setHealth(player.maxHealth)
-		print("Player hp went up to " + str(player.maxHealth))
-	player.get_node("PlayerAudio").stream = load("res://Audio/RecievedChat.ogg")
-	player.get_node("PlayerAudio").playing = true
-	player.get_node("PlayerAudio").volume_db = Global.masterSound
-	if not respawnable:
-		Global.destroyedObjects.append(Global.currentScene+name)
-	queue_free()
+	if not collected:
+		collected = true
+		player.piecesOfHeart += 1
+		emit_signal("collected")
+		print("Found a piece of heart!")
+		if player.piecesOfHeart == 3:
+			player.piecesOfHeart = 0
+			player.maxHealth += 1
+			player.setHealth(player.maxHealth)
+			print("Player hp went up to " + str(player.maxHealth))
+		player.get_node("PlayerAudio").stream = load("res://Audio/RecievedChat.ogg")
+		player.get_node("PlayerAudio").playing = true
+		player.get_node("PlayerAudio").volume_db = Global.masterSound
+		if not respawnable:
+			Global.destroyedObjects.append(Global.currentScene+name)
+		queue_free()
