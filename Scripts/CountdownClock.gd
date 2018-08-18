40b30fd059610ebcdbf329797343c8ab6cd12a03
Struckdown <bfleysher@gmail.com>
2018-08-18 15:43:41 -0600

    Meet the Clock
    A Countdown clock has been added that can be used for... whatever we want

---
+++ Scripts/CountdownClock.gd
@@ -0,0 +1,36 @@
+extends Control
+export var seconds = 0
+var minutes = 0
+var milliSec = 0
+
+signal countdownFinished
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	while seconds > 60:
+		minutes += 1
+		seconds -= 60
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+
+	if seconds > 0:
+		seconds -= delta
+		if seconds <= 0 and minutes > 0:
+			seconds = 60
+			minutes -= 1
+	else:
+		seconds = 0
+		emit_signal("countdownFinished")
+	updateLabel()
+
+func updateLabel():
+	milliSec = seconds * 1000
+	var milliSecText
+	if seconds > 1:
+		milliSecText = ("%0.2f" % (milliSec)).substr(2, 2)
+	else:
+		milliSecText = ("%0.2f" % (milliSec)).substr(0, 2)
+	$label.text = "%02.0d" % minutes + ":" + "%02.0d" % seconds + ":" + milliSecText
