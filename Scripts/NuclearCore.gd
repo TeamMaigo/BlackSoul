26950945b622beea759a9c7b86ef30a305576f05
Struckdown <bfleysher@gmail.com>
2018-08-21 21:16:18 -0600

    The Core Escape Sequence is Fancier
    I've added in some lighting, some audio, made the timer reset the
    sequence and even added some shaking. It's looking good

--- Scripts/NuclearCore.gd
+++ Scripts/NuclearCore.gd
@@ -1,6 +1,8 @@
 extends "res://Scripts/Barrel.gd"
 
 onready var countdownTimer = get_tree().get_root().get_node("World/CanvasLayer/CountdownClock")
+onready var player = get_tree().get_root().get_node("World/Player")
+export var countdownSeconds = 90
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -11,6 +13,14 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+func takeDamage(dmg):
+	health -= dmg
+	player.trauma = 120
+	audioPlayer.stream = load("res://Audio/temp_wood.wav")
+	audioPlayer.volume_db = Global.masterSound
+	audioPlayer.play()
+	if health <= 0 && !destroyed:
+		destroy()
 
 func destroy():
 	if not respawnable:
@@ -24,7 +34,10 @@ func destroy():
 	countdownTimer.paused = false
 	countdownTimer.show()
 	get_node("../MeltingText").show()
+	countdownTimer.seconds = countdownSeconds
+	countdownTimer.start()
 	emit_signal("destroyed")
+	countdownTimer.waitToShake(5)
 
 func _on_audioStreamPlayer2D_finished():
 	queue_free()
