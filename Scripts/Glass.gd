4c6cf16e79bd0db909e1e77229e5c7937e3121ef
Struckdown <bfleysher@gmail.com>
2018-08-27 22:46:14 -0600

    Fixed glass respawning
    Fixed glass respawning, moved a switch away from a gate

--- Scripts/Glass.gd
+++ Scripts/Glass.gd
@@ -13,6 +13,8 @@ enum PALETTETYPE { lab,acid,core }
 export(PALETTETYPE) var paletteType = PALETTETYPE.lab
 
 func _ready():
+	if Global.currentScene+name in Global.destroyedObjects:
+		queue_free()
 	if paletteType == PALETTETYPE.lab:
 		$sprite.texture = load("res://Sprites/BREAKABLE_BLUE.png")
 		$particles2D.texture = load("res://Sprites/BREAKABLE_SHARD_BLUE.png")
@@ -32,10 +34,12 @@ func shatterParams(pos, rot, speed):
 func takeDamage(dmg):
 	health -= dmg
 	if health <= 0:
+		if not respawnable:
+			Global.destroyedObjects.append(Global.currentScene+name)
 		$sprite.hide()
 		$collisionShape2D.disabled = true
 		$particles2D.process_material.initial_velocity = clamp(100 + 8.5*(shatterSpeed - 8), 100, 200)
-		#print($particles2D.process_material.initial_velocity)
+
 		$particles2D.emitting = true
 		$particles2D.rotation = shatterRot
 		
@@ -44,8 +48,6 @@ func takeDamage(dmg):
 		$audioStreamPlayer.play()
 		$animationPlayer.play("particleFadeout")
 		destroyed = true
-
-#func _process(delta):
 
 func _on_animationPlayer_animation_finished(anim_name):
 	if anim_name == "particleFadeout":
