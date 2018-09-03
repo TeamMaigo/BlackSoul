8f1d461135ba188e81ed7b95e4a2becea7aa5714
Struckdown <bfleysher@gmail.com>
2018-09-03 12:25:15 -0600

    New art imported
    Added in Save point art and animations
    Added swappable dummy art and idle animation
    Barrier has new art

--- Scripts/savePoint.gd
+++ Scripts/savePoint.gd
@@ -1,20 +1,31 @@
 extends Node2D
 
+enum PALETTETYPE { lab,acid,core }
+export(PALETTETYPE) var paletteType = PALETTETYPE.lab
+
 func _ready():
-	pass
+	# Initialization here
+	if paletteType == PALETTETYPE.lab:
+		$Sprite.texture = load("res://Sprites/SAVE_POINT.png")
+	if paletteType == PALETTETYPE.acid:
+		$Sprite.texture = load("res://Sprites/SAVE_POINT_ACID.png")
+	if paletteType == PALETTETYPE.core:
+		$Sprite.texture = load("res://Sprites/SAVE_POINT.png")
 
 func _on_Save_pressed(fileNumber):
 	get_tree().paused = false
 	Global.save_game(fileNumber)
 	$CanvasLayer/Options.hide()
+	$animationPlayer.play("Saving")
 
 
 func _on_Quit_pressed():
 	get_tree().paused = false
 	$CanvasLayer/Options.hide()
 
-func _on_SavePoint_body_entered(body):
+func _on_area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		body.setHealth(body.maxHealth)
 		$CanvasLayer/Options.show()
 		get_tree().paused = true
+
